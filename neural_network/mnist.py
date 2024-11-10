261877808f4f1a194d965d702adad31f6c0f0658
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-10 22:31:11 +0900

    tidy

--- neural_network/mnist.py
+++ neural_network/mnist.py
@@ -8,16 +8,16 @@ def main():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    model = Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        Dense(128, activation="relu"),
-        Dense(10, activation="softmax")
-    ])
+    model = Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            Dense(128, activation="relu"),
+            Dense(10, activation="softmax"),
+        ]
+    )
 
     model.compile(
-        optimizer="adam",
-        loss="sparse_categorical_crossentropy",
-        metrics=["accuracy"]
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
     )
 
     model.fit(x_train, y_train, epochs=5)
