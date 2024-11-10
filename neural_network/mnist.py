dae75accf92d430b4310ef842b1b35953b13d04d
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-10 22:20:57 +0900

    add mnist.py

---
+++ neural_network/mnist.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+
+def main():
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        Dense(128, activation="relu"),
+        Dense(10, activation="softmax")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    model.fit(x_train, y_train, epochs=5)
+
+    print(model.evaluate(x_test, y_test))
+
+
+main()
